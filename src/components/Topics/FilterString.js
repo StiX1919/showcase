db706a9cccb453f5cb27d8b3e9fc1af5b5158d73
Spencer Smith <WankFerret@Stevens-MacBook.local>
2017-10-13 22:27:13 -0500

    One more down

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,11 +1,45 @@
 import React, {Component} from "react"
 
 class FilterString extends Component {
+    constructor(){
+
+        super()
+        this.state = {
+        unfilteredArray: ['James','Jessica','Melody','Tyler','Blake','Jennifer','Mark','Maddy'],
+        userInput: "",
+        filteredArray: []
+
+
+        }
+    }
+
+    handleState(val){
+        this.setState({userInput: val})
+    }
+
+    filterMethod(userInput) {
+        var unfiltered = this.state.unfilteredArray
+        var newArr = [];
+
+        for(var i=0;i<unfiltered.length;i++){
+            if (unfiltered[i].toLowerCase().includes(userInput.toLowerCase())){
+                newArr.push(unfiltered[i])
+            }
+        }
+        this.setState({filteredArray: newArr})
+    }
+
+
     render(){
         return(
-            <p>FilterString Component</p>
+            <div className="puzzleBox filterObjectPB">
+                <h4>Filter Object</h4>
+                <span className="puzzleText" > Original: { JSON.stringify(this.state.unfilteredArray) }</span>
+                <input className="inputLine" onChange={(e)=> this.handleState(e.target.value)}></input>
+                <button className="confirmationButton" onClick={()=>this.filterMethod(this.state.userInput)}>Filter</button>
+                <span className="resultsBox filterObjectRB">{ JSON.stringify(this.state.filteredArray) }</span>
+            </div>
         )
     }
 }
-
 export default FilterString
